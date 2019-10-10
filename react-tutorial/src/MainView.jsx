13420a977d50177adf496add3038b5a8f652cb59
Dejan-24 <sarajlic24dejan@gmail.com>
2019-10-10 15:49:46 +0200

    Added new view

---
+++ react-tutorial/src/MainView.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainView extends Component {
+    render() {
+        return (
+            <div>
+                aaa
+            </div>
+        )
+    }
+}
